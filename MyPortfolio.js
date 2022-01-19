8e28a62cbeaba09196c5041a418efda4db914a00
SKShehan <shehansilva2013@gmail.com>
2022-01-19 16:53:15 +0530

    .
    .

--- MyPortfolio.js
+++ MyPortfolio.js
@@ -1,16 +1,31 @@
-var counter = document.getElementById("webC");
-var visitCount = localStorage.getItem("page_view");
-console.log("Shit Works");
+// var counter = document.getElementById("webC");
+// var visitCount = localStorage.getItem("page_view");
+// console.log("Shit Works");
 
-if(visitCount) {
-    visitCount = Number(visitCount) + 1;
-    localStorage.setItem("page_view" , visitCount);
+// if(visitCount) {
+//     visitCount = Number(visitCount) + 1;
+//     localStorage.setItem("page_view" , visitCount);
+// }
+
+// else {
+//     visitCount = 1;
+//     localStorage.setItem("page_view" ,1 );
+// }
+
+// counter.innerHTML = visitCount;
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for(var i = 0; i<reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
 }
 
-else {
-    visitCount = 1;
-    localStorage.setItem("page_view" ,1 );
-}
-
-counter.innerHTML = visitCount;
-
+window.addEventListener("scroll" , reveal);
